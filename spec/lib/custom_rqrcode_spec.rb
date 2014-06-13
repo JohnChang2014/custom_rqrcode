fb7ae286d668dc6f7efa0a4d607048ebe288692e
John Chang <johnchang2014@gmail.com>
2014-06-12 23:23:51 -0400

    refactored example group with let (:cqrcode)

--- spec/lib/custom_rqrcode_spec.rb
+++ spec/lib/custom_rqrcode_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
 describe RQRCode::CQRCode do
-	context ".generateQRcode" do		
+	context ".generateQRcode" do
+		let (:cqrcode) { RQRCode::CQRCode.new }	
+			
 		context "with default options but different format" do
 			it "returns qrcode in svg format" do
-				cqrcode = RQRCode::CQRCode.new
 				result = cqrcode.generateQRcode
 				expect(result).to match(/svg/)
 			end
 			
 			it "returns qrcode in png format with :png" do
 				expect {
-					cqrcode = RQRCode::CQRCode.new
 					cqrcode.generateQRcode({}, :png)
 				}.to_not raise_error
 			end
+			
 		end
 	end
 end
